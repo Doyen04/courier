103ad18cc78b329a9ef8870011ff7f60e1d90fec
Doyen <101886678+Doyen04@users.noreply.github.com>
2026-09-25 12:21:19 +0100

    style: comment out register function in instrumentation.ts for future use

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,6 +1,8 @@
-export async function register() {
-    if (process.env.NEXT_RUNTIME === "nodejs") {
-        const { getEnv } = await import("@/lib/env");
-        getEnv();
-    }
-}
+//  uuncomment when done with app
+
+// export async function register() {
+//     if (process.env.NEXT_RUNTIME === "nodejs") {
+//         const { getEnv } = await import("@/lib/env");
+//         getEnv();
+//     }
+// }
